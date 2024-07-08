d50ea33274e998998bb0792457f1fae0b9b51b69
Damandeep-S <sdaman705@gmail.com>
2024-07-08 16:38:34 +0530

    Added video routes and made links to controller

--- src/app.js
+++ src/app.js
@@ -15,8 +15,9 @@ app.use(cookieParser())
 //routes import
 
 import userRouter from './routes/user.routes.js'
-
+import videoRouter from './routes/video.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/videos", videoRouter)
 
 export {app}
---
+++ src/routes/video.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { publishAVideo } from "../controllers/video.controller.js";
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/").post(
+    upload.fields([
+        {
+          name: "videoFile",
+          maxCount: 1,
+        },
+        {
+          name: "thumbnail",
+          maxCount: 1,
+        },
+      ]),
+      publishAVideo
+)
+
+export default router
