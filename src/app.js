7a9619ac775049839ac0276ac740b4853dca0c61
Damandeep-S <sdaman705@gmail.com>
2024-07-14 14:08:50 +0530

    likes api routes

--- src/app.js
+++ src/app.js
@@ -19,11 +19,13 @@ import videoRouter from './routes/video.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import subscriptionRouter from "./routes/subscription.routes.js"
+import likeRouter from './routes/like.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/playlist",playlistRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/likes", likeRouter)
 
 export {app}
